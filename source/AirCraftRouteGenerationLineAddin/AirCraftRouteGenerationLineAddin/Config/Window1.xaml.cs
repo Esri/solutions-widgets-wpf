e93de5f5bdad65ce37883b31ba0ec4e2e2f4d769
Jo Fraley <mjfraley@esri.com>
2013-07-16 10:15:19 -0400

    Aircraft Route Generation Line Addin

---
+++ source/AirCraftRouteGenerationLineAddin/AirCraftRouteGenerationLineAddin/Config/Window1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AirCraftRouteGenerationLineAddin.Config
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public string serviceURL { get; private set; }
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            serviceURL = ServiceTextBox.Text;
+            DialogResult = true;
+
+        }
+    }
+}
