227c5f2083bdc2180e98174815fc4248b286548d
Jo Fraley <mjfraley@esri.com>
2013-07-16 10:29:29 -0400

    Farthest On Circle Addin for 10.2 build 294

---
+++ source/FarthestOnCircleAddin_102/FarthestOnCircleAddin_102/Config/FarthestOnCircleDialog.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FarthestOnCircleAddin_102.Config
+{
+
+    /// <summary>
+    /// Interaction logic for FarthestOnCircleDialog.xaml
+    /// </summary>
+    /// 
+    public partial class FarthestOnCircleDialog : Window
+    {
+        public string serviceURL { get; private set; }
+
+        public FarthestOnCircleDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            serviceURL = ServiceTextBox.Text;
+            DialogResult = true;
+
+        }
+
+    }
+}
