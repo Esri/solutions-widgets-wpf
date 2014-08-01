7e13d57069decaddb9fb7c78bf3f679f099ccafe
Jo Fraley <jfraley@esri.com>
2014-08-01 16:03:02 -0400

    New widget to show feeds from cameras

---
+++ source/LiveWebCams/LiveWebCams/Config/WebCamWidgetDialog.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using ESRI.ArcGIS.OperationsDashboard;
+using client = ESRI.ArcGIS.Client;
+
+namespace LiveWebCams.Config
+{
+    /// <summary>
+    /// Interaction logic for WebCamWidgetDialog.xaml
+    /// </summary>
+    public partial class WebCamWidgetDialog : Window
+    {
+
+        public DataSource DataSource { get; private set; }
+        public string cameraURL { get; private set; }
+        public string Caption { get; private set; }
+
+        public WebCamWidgetDialog(IList<DataSource> dataSources, string initialCaption, string initialDataSourceId, string cameraurl)
+        {
+            InitializeComponent();
+
+            // When re-configuring, initialize the widget config dialog from the existing settings.
+            CaptionTextBox.Text = initialCaption;
+            if (!string.IsNullOrEmpty(initialDataSourceId))
+            {
+                DataSource dataSource = OperationsDashboard.Instance.DataSources.FirstOrDefault(ds => ds.Id == initialDataSourceId);
+                URLTextBox.Text = cameraurl;
+            }
+        }
+
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataSource = DataSourceSelector.SelectedDataSource;
+            Caption = CaptionTextBox.Text;
+            cameraURL = URLTextBox.Text;
+
+            DialogResult = true;
+        }
+
+        private void DataSourceSelector_SelectionChanged(object sender, EventArgs e)
+        {
+            DataSource dataSource = DataSourceSelector.SelectedDataSource;
+
+        }
+
+        private void ValidateInput(object sender, TextChangedEventArgs e)
+        {
+            if (OKButton == null)
+                return;
+
+            OKButton.IsEnabled = false;
+            if (string.IsNullOrEmpty(CaptionTextBox.Text))
+                return;
+
+            OKButton.IsEnabled = true;
+        }
+
+
+    }
+}
---
+++ source/LiveWebCams/LiveWebCams/WebCamWidget.xaml.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using ESRI.ArcGIS.OperationsDashboard;
+using client = ESRI.ArcGIS.Client;
+
+namespace LiveWebCams
+{
+    /// <summary>
+    /// A Widget is a dockable add-in class for Operations Dashboard for ArcGIS that implements IWidget. By returning true from CanConfigure, 
+    /// this widget provides the ability for the user to configure the widget properties showing a settings Window in the Configure method.
+    /// By implementing IDataSourceConsumer, this Widget indicates it requires a DataSource to function and will be notified when the 
+    /// data source is updated or removed.
+    /// </summary>
+    [Export("ESRI.ArcGIS.OperationsDashboard.Widget")]
+    [ExportMetadata("DisplayName", "Webcam Widget")]
+    [ExportMetadata("Description", "Displays an image, such as from a webcam. The image refreshes with each heartbeat of the dashboard.")]
+    [ExportMetadata("ImagePath", "/LiveWebCams;component/Images/webcam.jpg")]
+    [ExportMetadata("DataSourceRequired", true)]
+    [DataContract]
+    public partial class WebCamWidget : UserControl, IWidget, IDataSourceConsumer
+    {
+        /// <summary>
+        /// A unique identifier of a data source in the configuration. This property is set during widget configuration.
+        /// </summary>
+        [DataMember(Name = "dataSourceId")]
+        public string DataSourceId { get; set; }
+
+        /// <summary>
+        /// The name of a field within the selected data source. This property is set during widget configuration.
+        /// </summary>
+        [DataMember(Name = "camera")]
+        public string Camera { get; set; }
+
+        public WebCamWidget()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateControls()
+        {
+            try
+            {
+                updateImage();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        #region IWidget Members
+
+        private string _caption = "Default Caption";
+        /// <summary>
+        /// The text that is displayed in the widget's containing window title bar. This property is set during widget configuration.
+        /// </summary>
+        [DataMember(Name = "caption")]
+        public string Caption
+        {
+            get
+            {
+                return _caption;
+            }
+
+            set
+            {
+                if (value != _caption)
+                {
+                    _caption = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// The unique identifier of the widget, set by the application when the widget is added to the configuration.
+        /// </summary>
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// OnActivated is called when the widget is first added to the configuration, or when loading from a saved configuration, after all 
+        /// widgets have been restored. Saved properties can be retrieved, including properties from other widgets.
+        /// Note that some widgets may have properties which are set asynchronously and are not yet available.
+        /// </summary>
+        public void OnActivated()
+        {
+            UpdateControls();
+        }
+
+        /// <summary>
+        ///  OnDeactivated is called before the widget is removed from the configuration.
+        /// </summary>
+        public void OnDeactivated()
+        {
+        }
+
+        /// <summary>
+        ///  Determines if the Configure method is called after the widget is created, before it is added to the configuration. Provides an opportunity to gather user-defined settings.
+        /// </summary>
+        /// <value>Return true if the Configure method should be called, otherwise return false.</value>
+        public bool CanConfigure
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        ///  Provides functionality for the widget to be configured by the end user through a dialog.
+        /// </summary>
+        /// <param name="owner">The application window which should be the owner of the dialog.</param>
+        /// <param name="dataSources">The complete list of DataSources in the configuration.</param>
+        /// <returns>True if the user clicks ok, otherwise false.</returns>
+        public bool Configure(Window owner, IList<DataSource> dataSources)
+        {
+            // Show the configuration dialog.
+            Config.WebCamWidgetDialog dialog = new Config.WebCamWidgetDialog(dataSources, Caption, DataSourceId, Camera) { Owner = owner };
+            if (dialog.ShowDialog() != true)
+                return false;
+
+            // Retrieve the selected values for the properties from the configuration dialog.
+            Caption = dialog.Caption;
+            DataSourceId = dialog.DataSource.Id;
+            Camera = dialog.URLTextBox.Text;
+
+            // The default UI simply shows the values of the configured properties.
+            UpdateControls();
+
+            return true;
+
+        }
+
+        #endregion
+
+        #region IDataSourceConsumer Members
+
+        /// <summary>
+        /// Returns the ID(s) of the data source(s) consumed by the widget.
+        /// </summary>
+        public string[] DataSourceIds
+        {
+            get { return new string[] { DataSourceId }; }
+        }
+
+        /// <summary>
+        /// Called when a DataSource is removed from the configuration. 
+        /// </summary>
+        /// <param name="dataSource">The DataSource being removed.</param>
+        public void OnRemove(DataSource dataSource)
+        {
+            // Respond to data source being removed.
+            DataSourceId = null;
+        }
+
+        /// <summary>
+        /// Called when a DataSource found in the DataSourceIds property is updated.
+        /// </summary>
+        /// <param name="dataSource">The DataSource being updated.</param>
+        public void OnRefresh(DataSource dataSource)
+        {
+            // If required, respond to the update from the selected data source.
+            // Consider using an async method.
+            updateImage();
+        }
+        private void updateImage()
+        {
+            var bitmap = new BitmapImage();
+            System.Net.WebClient webClient = new System.Net.WebClient();
+
+            using (var stream = new MemoryStream(webClient.DownloadData(Camera)))
+            {
+                bitmap.BeginInit();
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.StreamSource = stream;
+                bitmap.EndInit();
+            }
+
+            bitmap.Freeze();
+            ImageBox.Dispatcher.Invoke((Action)(() => ImageBox.Source = bitmap));
+        }
+
+        #endregion
+    }
+}
